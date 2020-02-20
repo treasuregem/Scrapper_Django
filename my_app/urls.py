916e2f909c4d5ee62a419967d08dee5dcc3b89bd
treasuregem <hemendrakumar86@gmail.com>
2020-02-20 23:50:02 +0530

    add frontend

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # When only domain name is provided, check home view in views.py to carryout the action
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('new_search', views.new_search, name='new_search'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
-
+from bs4 import BeautifulSoup
+import requests
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+
+# Create view for search
+def new_search(request):
+    search = request.POST.get('search')
+    # print(search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
